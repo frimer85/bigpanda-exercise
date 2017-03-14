024d24586feaf39121dfe3467719bf498e102472
frimer85 <frimer85@gmail.com>
2017-03-14 16:48:39 +0200

    Adding the 2 python services .py files

---
+++ roles/panda/counter-panda.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+postCount=0
+app=Flask(__name__)
+
+@app.before_request
+def before_request():
+    if request.method == "POST":
+        global postCount
+        postCount += 1
+
+
+@app.route('/',methods=['GET', 'POST'])
+def home():
+    global postCount
+    return "Number of POST requests: " + str(postCount) + "\n"
+
+if __name__ == "__main__":
+    app.run()

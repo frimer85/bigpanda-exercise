35d4f985bae1db974651ac0f3e43f1984d2c577a
frimer85 <frimer85@gmail.com>
2017-03-15 17:01:12 +0200

    Adding services

--- roles/gify-panda/files/panda-app/gify-panda.py
+++ roles/gify-panda/files/panda-app/gify-panda.py
@@ -2,6 +2,7 @@ import os.path
 from flask import Flask
 from flask_autoindex import AutoIndex
 
+pm = __import__("gify-panda")
 app=Flask(__name__)
 AutoIndex(app, browse_root="/tmp/panda-app/resources")
 

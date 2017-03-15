7f0c767d48f05c85ebe2c9c95396310a4a0eda63
frimer85 <frimer85@gmail.com>
2017-03-15 22:59:49 +0200

    Adding deployment script

---
+++ deploy_panda_service.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import os
+import argparse
+
+__author__ = 'Moran Frimer'
+
+
+def get_args():
+    '''This function parses and return arguments passed in'''
+    # Assign description to the help doc
+    parser = argparse.ArgumentParser(
+        description='Deployment script for python nanoservices: gify-panda and counter-panda')
+    # Add arguments
+    parser.add_argument(
+        '-s', '--service', type=str, help='Service Name', required=True)
+    parser.add_argument(
+        '-b', '--branch', type=str, help='Branch to deploy', required=True)
+    # Array for all arguments passed to script
+    args = parser.parse_args()
+    # Assign args to variables
+    service = args.service
+    branch = args.branch
+    # Return all variable values
+    return service, branch
+
+service, branch = get_args()
+
+
+os.system("git fetch")
+os.system("git checkout " + branch)
+
+os.system("vagrant up --no-provision")
+os.system('ansible-playbook --private-key=.vagrant/machines/base/virtualbox/private_key -u vagrant -i dev/hosts -e "service=' + service + '" base.yml')
